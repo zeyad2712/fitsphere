73d135a42107430e0cb870dfe971d67a33ebbc23
zeyad2712 <zezowaleed2712@gmail.com>
2026-04-09 15:35:36 +0200

    feat: implement authentication pages and configure application routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import SignUp from './pages/ِAuth/SignUp';
-import Login from './pages/ِAuth/Login';
+import SignUp from './pages/Auth/SignUp';
+import Login from './pages/Auth/Login';
 import Home from './pages/Home';
 
 const App = () => {
